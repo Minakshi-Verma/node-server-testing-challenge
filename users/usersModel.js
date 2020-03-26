4332ba7e2d8c7ae327ba3c1d0355657dc6f67970
Minakshi Verma <mvedwan@gmail.com>
2020-03-26 16:55:17 -0400

    "endpoints inuserRouter are working"

--- users/usersModel.js
+++ users/usersModel.js
@@ -1,1 +1,29 @@
-const db = require("../")
+const db = require("../data/db-config")
+
+module.exports = {
+    getAll,
+    getById,
+    remove
+}
+
+//----getAll---
+function getAll(){
+return db("users")
+}
+
+//----getById---
+
+function getById(id){
+    return db('users')
+    .where({id})
+    .first()
+
+}
+
+//----delete-----
+
+function remove(id){
+    return db("users")
+    .where({id})
+    .del()
+}
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -5,10 +5,43 @@ Users = require('./usersModel')
 const router = express.Router()
 
 //GET request
+router.get('/', (req,res)=>{
+  Users.getAll()
+  .then(users=>{
+      res.status(200).json(users)
+  })
+  .catch(err=>{
+      res.status(500).json(error)
+  })
+})
 
 
 //GET by ID request
 
+router.get('/:id', (req,res)=>{
+    const {id} = req.params
+
+    Users.getById(id)
+    .then(user=>{
+        res.status(200).json(user)
+    })
+    .catch(err=>{
+        res.status(500).json({message:"We can not find the user with this id"})
+    })
+})
+
 //DELETE request
+
+router.delete('/:id', (req,res)=>{
+    const {id} = req.params
+    Users.remove(id)
+    .then(deleted=>{
+        res.status(204).json({message:"No data available"})
+    })
+    .catch(err=>{
+        res.status(500).json(err)
+    })
+})
+
 
 module.exports = router;
